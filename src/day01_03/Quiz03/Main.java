0f084c1f93aacae54e27d6529a414fa0ec40c6aa
Ansai-2000 <dudgns3681@gmail.com>
2024-01-05 19:24:21 +0900

    엘리스트랙 백엔드 자바 공부

--- src/day01_03/Quiz03/Main.java
+++ src/day01_03/Quiz03/Main.java
@@ -1,66 +1,46 @@
 package day01_03.Quiz03;
 
-abstract class Plan {
-    private int time;
-    int price;
-
-    Plan(int t, int p) {
-        this.time = t;
-        price = p;
+// 운동선수 인터페이스
+interface Athlete{
+    void train();
+    default void compete(){
+        System.out.println("경쟁");
     }
-
-    private void changeState() {
-        this.time -= 1;
-        price -= 1;
-    }
-
-    void complete() {
-        this.time = 0;
-    }
-
-    private void checkState() {
-        System.out.println("남은 예산은 " + price + "이며 " + time + " 기간 소요될 것으로 보입니다");
-    }
-
-    public void processing() {
-        changeState();
-        if(price <= 0) {
-            System.out.println("\n!예산이 부족합니다!");
-        } else if(this.time <= 0) {
-            System.out.println("\n!작업이 완료 되었습니다!");
-        } else {
-            System.out.println("\n 작업 진행중.. ");
-            checkState();
-        }
-    }
-
-    void upgrading() {};
-}
-
-class DevelopedPlan extends Plan {
-
-    DevelopedPlan(int t, int p) {
-        super(t, p);
-    }
-
-    void upgrading() {
-        super.complete();
-        System.out.println("\n작업을 바로 완료합니다!");
+    static void rest(){
+        System.out.println("휴식");
     }
 }
 
+// 축구선수 클래스
+class SoccerPlayer implements Athlete{
+    public void train(){
+        System.out.println("축구 훈련");
+    }
+    public void compete(){
+        System.out.println("축구 경쟁");
+    }
+}
 
-// main에 있는 코드를 수정하면 채점에 오류가 생길 수 있습니다!
+// 야구선수 클래스
+class BaseballPlayer implements Athlete{
+    public void train(){
+        System.out.println("야구 훈련");
+    }
+
+
+}
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-        DevelopedPlan a = new DevelopedPlan(5, 10);
-        a.processing();
-        a.processing();
-        a.processing();
-        a.upgrading();
-        a.processing();
+        SoccerPlayer soccerPlayer = new SoccerPlayer();
+        soccerPlayer.train();
+        soccerPlayer.compete();
 
+        BaseballPlayer baseballPlayer = new BaseballPlayer();
+        baseballPlayer.train();
+        baseballPlayer.compete();
 
+        Athlete.rest();
     }
 }
