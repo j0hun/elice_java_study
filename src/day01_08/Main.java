fc5f332a54edaf2dd440c37b43650063c60f1557
Ansai-2000 <dudgns3681@gmail.com>
2024-01-10 23:42:21 +0900

    엘리스트랙 백엔드 자바 공부

--- src/day01_08/Main.java
+++ src/day01_08/Main.java
@@ -1,4 +1,40 @@
 package day01_08;
 
+import java.util.*;
+
+class MyStack {
+    // 여기에 코드를 작성하세요.
+    ArrayList<String> stackList = new ArrayList<>();
+    void push(String s){
+        stackList.add(s);
+    }
+    String pop(){
+        if(stackList.size() == 0){
+            return "-1";
+        }
+        String s = stackList.get(stackList.size() - 1);
+        stackList.remove(s);
+        return s;
+    }
+    int size(){
+        return stackList.size();
+    }
+    boolean isEmpty(){
+        return stackList.size() == 0;
+    }
+}
+
 public class Main {
+    public static void main(String args[]) {
+        // 채점을 위한 코드이므로 수정하지 말아주세요.
+        MyStack myStack = new MyStack();
+        if (myStack.isEmpty())   System.out.println("스택이 비어있습니다.");
+        myStack.push("3");
+        myStack.push("5");
+        System.out.println(myStack.size());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+    }
+
 }
