52052154cd4f951e8d13e3e40f12a664426245a0
Ansai-2000 <dudgns3681@gmail.com>
2024-01-12 16:29:23 +0900

    엘리스트랙 백엔드 자바 공부

---
+++ src/day01_08/Quiz08/Main.java
@@ -0,0 +1,31 @@
+package day01_08.Quiz08;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // 채점을 위한 코드이므로 수정하지 말아주세요.
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> loginInfo = new HashMap<>();
+        loginInfo.put("abc", "abc123");
+        loginInfo.put("elice", "1234");
+        loginInfo.put("cheshire", "erihsehc");
+
+        // 여기에 코드를 작성해 주세요.
+        String id = scanner.nextLine();
+        String pw = scanner.nextLine();
+
+        if(loginInfo.containsKey(id)){
+            if(loginInfo.get(id).equals(pw)){
+                System.out.println("로그인에 성공했습니다.");
+            }
+            else{
+                System.out.println("비밀번호가 틀렸습니다.");
+            }
+        }else{
+            System.out.println("회원가입이 필요합니다.");
+        }
+    }
+
+}
